a98adfb6f43079f8be39a4d4ec80f6bc3d8e3de1
Daffabot <daffabot@programmer.net>
2024-05-18 12:16:50 +0700

    Delete js directory

--- js/utility/donate.js
+++
@@ -1,8 +0,0 @@
-function openGitHub() {
-    window.open("https://github.com/Daffabot");
-  }
-  function openDonate() {
-    window.open("https://paypal.me/daffabot");
-  }
-
-export {openDonate, openGitHub};
